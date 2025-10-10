d344169cc16ff28b175a53434fca57b1e1cb25d0
NiRuinan <niruinan@foxmail.com>
2025-10-10 23:19:05 +0800

    Delete 08.GWAS directory

--- 08.GWAS/GWAS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-### GWAS, Taking SNP as an example
-
-gmatrix --bfile SNPcleanuseful --grm agrm --out G
-uvlmm --bfile SNPcleanuseful --data data.txt \
-      --grm G --trait t1 --class sex,batch --out result_t1
